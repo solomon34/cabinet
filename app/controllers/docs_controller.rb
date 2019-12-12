b40db38d954c9bb6b26f47701abac3b33f29e898
HGPMAC 144 <hgpmac144@Thes-MacBook-Air-9.local>
2019-12-11 18:15:46 -0800

    Edit and Destroy Docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -24,10 +24,17 @@ class DocsController < ApplicationController
 	def edit 
 	end 
 
-	def update 
+	def update  
+		if @doc.update(doc_params) 
+			redirect_to @doc 
+		else
+			render'edit' 
+		end
 	end
 
-	def destroy 
+	def destroy  
+		@doc.destroy 
+		redirect_to docs_path
 	end 
 
 	private  
